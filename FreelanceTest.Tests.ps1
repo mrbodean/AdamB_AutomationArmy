55edbb03c7a32d9cb99957ad1d79bf51eb1173d2
Jon aka @MrBoDean <jon.warnken@gmail.com>
2017-07-06 17:34:43 -0400

    Created freelancetest files

---
+++ FreelanceTest.Tests.ps1
@@ -0,0 +1,85 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "FreelanceTest" {
+    #Prepare the TestDrive 
+    new-item -path "TestDrive:\Users" -ItemType directory
+    new-item -path "TestDrive:\Users\U1" -ItemType directory
+    new-item -path "TestDrive:\Users\U1\Folder" -ItemType directory
+    new-item -path "TestDrive:\Users\U1\Folder\file.txt" -ItemType file
+    new-item -path "TestDrive:\Users\U1\Folder\file.doc" -ItemType file
+    new-item -path "TestDrive:\Users\U1\Foo" -ItemType directory
+    new-item -path "TestDrive:\Users\U1\Foo\file.doc" -ItemType file
+
+    new-item -path "TestDrive:\Users\U2" -ItemType directory
+    new-item -path "TestDrive:\Users\U2\Foo" -ItemType directory
+    new-item -path "TestDrive:\Users\U2\Foo\file.doc" -ItemType file
+
+    new-item -path "TestDrive:\Users2" -ItemType directory
+    new-item -path "TestDrive:\Users2\U1" -ItemType directory
+    new-item -path "TestDrive:\Users2\U1\Bar" -ItemType directory
+    new-item -path "TestDrive:\Users2\U1\Bar\file.xxx" -ItemType file
+
+    new-item -path "TestDrive:\Users2\U3" -ItemType directory
+    new-item -path "TestDrive:\Users2\U3\Baz" -ItemType directory
+    new-item -path "TestDrive:\Users2\U3\Baz\file.abc" -ItemType file
+
+    new-item -path "TestDrive:\Users3" -ItemType directory
+    new-item -path "TestDrive:\Users3\U3" -ItemType directory
+    new-item -path "TestDrive:\Users3\U3\U3" -ItemType directory
+    new-item -path "TestDrive:\Users3\U3\U3\Foo" -ItemType directory
+    new-item -path "TestDrive:\Users3\U3\U3\Foo\file.txt" -ItemType file
+
+    new-item -path "TestDrive:\Users3\U1" -ItemType directory
+    new-item -path "TestDrive:\Users3\U1\U1" -ItemType directory
+    new-item -path "TestDrive:\Users3\U1\U1\Folder2" -ItemType directory
+    new-item -path "TestDrive:\Users3\U1\U1\Folder2\file.doc" -ItemType file
+    new-item -path "TestDrive:\Users3\U1\U1\Baz" -ItemType directory
+    new-item -path "TestDrive:\Users3\U1\U1\Baz\file.doc" -ItemType file
+
+    Context "Verify Test Setup" {
+        It "Check that all file paths exists" {
+            Test-Path "TestDrive:\Users\U1\Folder\file.txt"|Should Be $true
+            Test-Path "TestDrive:\Users\U1\Folder\file.doc"|Should Be $true
+            Test-Path "TestDrive:\Users\U1\Foo\file.doc"|Should Be $true
+            Test-Path "TestDrive:\Users\U2\Foo\file.doc"|Should Be $true
+            Test-Path "TestDrive:\Users2\U1\Bar\file.xxx"|Should Be $true
+            Test-Path "TestDrive:\Users2\U3\Baz\file.abc"|Should Be $true
+            Test-Path "TestDrive:\Users3\U3\U3\Foo\file.txt"|Should Be $true
+            Test-Path "TestDrive:\Users3\U1\U1\Folder2\file.doc"|Should Be $true
+            Test-Path "TestDrive:\Users3\U1\U1\Baz\file.doc"|Should Be $true
+        }
+    }
+    
+    $destination = "TestDrive:Destination"
+    $source = "TestDrive:\Users","TestDrive:\Users2","TestDrive:\Users3"
+    FreelanceTest -source $source -destination $destination -verbose
+
+    Context "Verify End Results" {
+        It "Check that all expected files are present" {
+            Test-Path "TestDrive:\Destination\U1\Foo\file.doc"|Should Be $true
+            Test-Path "TestDrive:\Destination\U1\Bar\file.xxx"|Should Be $true
+            Test-Path "TestDrive:\Destination\U1\Baz\file.doc"|Should Be $true
+            Test-Path "TestDrive:\Destination\U2\Foo\file.doc"|Should Be $true
+            Test-Path "TestDrive:\Destination\U3\Baz\file.abc"|Should Be $true
+        }
+        It "Verify there are only 5 files in the Destination" {
+            ((Get-ChildItem -Path "TestDrive:\Destination" -Recurse -file).count -eq 5)|Should Be $true   
+        }
+        It "Only copies the newest files" {
+            $newdestfile = "TestDrive:\Destination\U1\Foo\file.doc"
+            $newsourcefile = "TestDrive:\Users3\U1\U1\Baz\file.doc"
+            $newsource_destfile = "TestDrive:\Destination\U1\Baz\file.doc"
+            New-Item -Path $newdestfile -Force
+            New-Item -Path $newsourcefile -Force
+            $newdesttimestamp = (Get-ChildItem -Path $newdestfile).LastWriteTime
+            $newsourcetimestamp = (Get-ChildItem -Path $newsourcefile).LastWriteTime
+            $destination = "TestDrive:Destination"
+            $source = "TestDrive:\Users","TestDrive:\Users2","TestDrive:\Users3"
+            FreelanceTest -source $source -destination $destination -verbose
+            (Get-ChildItem -Path $newdestfile).LastWriteTime| Should Be $newdesttimestamp
+            (Get-ChildItem -Path $newsource_destfile).LastWriteTime| Should Be $newsourcetimestamp
+        }
+    }
+}
---
+++ FreelanceTest.ps1
@@ -0,0 +1,69 @@
+﻿function FreelanceTest {
+    [CmdletBinding()]
+    Param
+    (
+        #Source Path
+        [Parameter(Mandatory=$true)]
+        [string[]]$Source,
+        #Destination Path
+        [Parameter(Mandatory=$true)]
+        [string]$destination
+    )
+    Begin{
+        $Excludedfiletypes = "*.txt","*.pdf"
+        $Includeddirectories = "Foo","Bar","Baz"
+        if(!(Test-path -path $destination)){
+            Write-Verbose "$destination was not found it will be created"
+            try{New-Item -Path $destination -ItemType Directory}
+            catch{
+                Throw "Unable to create $destination"
+                return
+            }
+        }
+    }
+    Process{
+        Foreach($path in $Source){
+            Write-Verbose "Ensure $path is accessable"
+            If(Test-Path -Path $path){
+                $sourcedirectories = Get-ChildItem -Path $path -Include $Includeddirectories -Recurse -Directory
+                    Write-Verbose "Found $($sourcedirectories.count)"
+                $sourcefiles = Get-ChildItem -Path $sourcedirectories -Exclude $Excludedfiletypes -Recurse -File
+                foreach($file in $sourcefiles){
+                    Write-Verbose "processing $file"
+                    $2levelparent = $file.psparentpath.split('\')
+                    $count = $2levelparent.count
+                    $2levelparent = "$($2levelparent[($count-2)])\$($2levelparent[($count-1)])"
+                    Write-Verbose "Profile parent path is $2levelparent"
+                    $destinationpath = Join-Path -Path $destination -ChildPath $2levelparent
+                    if(!(Test-Path $destinationpath)){
+                        Write-Verbose "$destinationpath was not found. Creating the directory."
+                        New-Item -Path $destinationpath -ItemType Directory -Force
+                        }
+                    $destinationfilepath = Join-Path -Path $destinationpath -ChildPath $file.name
+                    if(Test-path -Path $destinationfilepath){
+                        Write-Verbose "$destinationfilepath was found. Check to see if the source is newer."
+                        $existingfile = Get-ChildItem -Path $destinationfilepath
+                        if($file.LastWriteTime -gt $existingfile.LastWriteTime){
+                            Write-Verbose "$($file.fullname) is newer and will be copied."
+                            try{Copy-Item -Path $file.fullname -Destination $destinationpath -Force}
+                            catch{
+                                Write-Error "Error copying $($file.fullname) to $destinationpath"
+                            }
+                        }else{
+                            Write-Verbose "$($existingfile.fullname) is newer and will not be replaced."
+                        }
+                    }else{
+                        Write-Verbose "$($file.fullname) will be copied to $destinationpath"
+                        try{Copy-Item -Path $file.fullname -Destination $destinationpath}
+                        catch{
+                            Write-Error "Error copying $($file.fullname) to $destinationpath"
+                        }
+                    }
+                }
+            }else{
+                Write-Warning "Unable to access $path !!! It will not be processed!!!"
+            }
+        }
+
+    }
+}
